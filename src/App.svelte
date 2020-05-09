7230294915342c5f03b7b6985cc55db7481c2894
Alan Reid <alanreidt@gmail.com>
2020-05-09 16:59:19 +0300

    Reorder code blocks in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,14 @@
   import InputField from './components/InputField.svelte';
 </script>
 
-<main>
-  <div class="row">
-    <InputField name="text-field" labelText="text field" placeholder="Email"/>
-  </div>
-</main>
-
 <style>
   .row {
     max-width: 320px;
   }
 </style>
+
+<main>
+  <div class="row">
+    <InputField name="text-field" labelText="text field" placeholder="Email" />
+  </div>
+</main>
