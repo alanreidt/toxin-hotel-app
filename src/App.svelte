59dc0c87840b6c6a8ede165ceaeae45a8fada30a
Alan Reid <alanreidt@gmail.com>
2020-05-09 13:35:30 +0300

    Create InputField component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
 <script>
+  import InputField from './components/InputField.svelte';
 </script>
 
 <main>
+  <div class="row">
+    <InputField labelText="Name:"/>
+  </div>
 </main>
 
 <style>
+  .row {
+    max-width: 320px;
+  }
 </style>
---
+++ src/components/InputField.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let labelText;
+</script>
+
+<style lang="scss">
+  .input-field {
+    display: inline-block;
+    vertical-align: middle;
+    width: 100%;
+
+    &__label {
+      display: inline-block;
+      vertical-align: middle;
+      width: 100%;
+    }
+  }
+
+  .input-field__input {
+    padding: 0;
+    display: inline-block;
+    vertical-align: middle;
+    width: 100%;
+    height: 2.75rem;
+    padding-left: 0.9375rem;
+    background-color: #fff;
+    outline: none;
+    border-radius: 0.25rem;
+    box-sizing: border-box;
+    transition: border-color 0.25s ease;
+  }
+
+  .input-field__input::placeholder {
+    opacity: 1;
+  }
+</style>
+
+<div class="input-field">
+  <label class="input-field__label">
+    {labelText}
+    <input class="input-field__input">
+  </label>
+</div>
