e9e2f251d7fca89b02bb5161924f3fe7b3541f07
Alan Reid <alanreidt@gmail.com>
2020-05-11 10:21:18 +0300

    Create DateInputField component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import InputField from './components/InputField.svelte';
+  import DateInputField from './components/DateInputField.svelte';
 </script>
 
 <style>
@@ -11,5 +12,6 @@
 <main>
   <div class="row">
     <InputField name="text-field" labelText="text field" placeholder="Email" />
+    <DateInputField name="date-text-field" labelText="masked text field" />
   </div>
 </main>
---
+++ src/components/DateInputField.svelte
@@ -0,0 +1,32 @@
+<script>
+  import InputField from "./InputField.svelte";
+  import Cleave from "cleave.js";
+
+  window.addEventListener("load", () => {
+    const now = new Date();
+    const nowISOString = now.toISOString();
+    const afterTwoYearsFromNow = new Date(
+      now.getFullYear() + 2,
+      now.getMonth(),
+      now.getDate()
+    );
+    const afterTwoYearsFromNowISOString = afterTwoYearsFromNow.toISOString();
+
+    const dateMasks = document.querySelectorAll(".js-date-mask");
+
+    dateMasks.forEach((dateMask) => {
+      new Cleave(dateMask, {
+        date: true,
+        delimiter: ".",
+        dateMin: nowISOString,
+        dateMax: afterTwoYearsFromNowISOString,
+        datePattern: ["d", "m", "Y"],
+      });
+    });
+  });
+</script>
+
+<InputField
+  class="input-field__input js-date-mask"
+  placeholder="ДД.ММ.ГГГГ"
+  {...$$props} />
