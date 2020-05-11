16f66c0ca0565e0b4ae9f6bb51d18ba6a58ba570
Alan Reid <alanreidt@gmail.com>
2020-05-11 13:33:28 +0300

    Convert DropdownMenu component

---
+++ src/components/DropdownMenu.svelte
@@ -0,0 +1,74 @@
+<script>
+  import Counter from "./Counter.svelte";
+  import Button from "./Button.svelte";
+
+  export let optionNames;
+  export let hasButtonPane = false;
+</script>
+
+<style lang="less">
+  @import "../styles/variables";
+  @import "../styles/mixins";
+
+  .dropdown-menu {
+    .h3-text();
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0.9375rem;
+    padding-right: 0.4375rem;
+
+    background-color: #fff;
+    border: 1px solid #colors[dark-shade-50];
+    border-bottom-left-radius: 0.25rem;
+    border-bottom-right-radius: 0.25rem;
+
+    box-sizing: border-box;
+
+    // transition: height 0.3s ease-out;
+
+    &__option {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      margin-bottom: 1.375rem;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    &__option-text {
+      display: inline-block;
+      vertical-align: middle;
+    }
+
+    &__button-pane {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      padding-top: 0.4375rem;
+    }
+  }
+</style>
+
+<div class="dropdown-menu">
+  {#each optionNames as optionName}
+    <div class="dropdown-menu__option">
+      <span class="dropdown-menu__option-text">{optionName}</span>
+      <div class="dropdown-menu__counter">
+        <Counter />
+      </div>
+    </div>
+  {/each}
+
+  {#if hasButtonPane}
+    <div class="dropdown-menu__button-pane">
+      <Button text="Очистить" mods="reset" />
+      <Button text="Применить" />
+    </div>
+  {/if}
+</div>
