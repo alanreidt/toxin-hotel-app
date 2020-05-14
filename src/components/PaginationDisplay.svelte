c5fe84f35f94c72b19df72d4228f5b7a019a3aa9
Alan Reid <alanreidt@gmail.com>
2020-05-14 22:14:44 +0300

    Convert PaginationDisplay component

---
+++ src/components/PaginationDisplay.svelte
@@ -0,0 +1,96 @@
+<script>
+</script>
+
+<style lang="less">
+  @import "../styles/variables";
+  @import "../styles/mixins";
+
+  .pagination-display {
+    &__list {
+      .list-styles-reset();
+
+      display: flex;
+      flex-flow: row nowrap;
+    }
+
+    &__link {
+      .body-text();
+      text-decoration: none;
+
+      display: inline-block;
+      vertical-align: middle;
+      width: 40px;
+      height: 40px;
+      line-height: 40px;
+
+      font-size: 12px;
+      text-align: center;
+      background-image: transparent;
+      color: #colors[dark-shade-50];
+      border-radius: 50%;
+
+      &:hover,
+      &:focus {
+        background-color: #colors[dark-shade-25];
+      }
+    }
+
+    &__link_active {
+      background-image: @primary-gradient;
+      color: #fff;
+
+      &:hover,
+      &:focus {
+        opacity: 0.5;
+      }
+    }
+
+    &__link_arrow {
+      // .arrow();
+      .decoration_type_arrow-forward();
+      &::before {
+        // styles for .decoration_type_arrow-forward();
+        width: inherit;
+        height: inherit;
+        line-height: inherit;
+        color: #fff;
+      }
+
+      background-image: @secondary-gradient;
+
+      &:hover,
+      &:focus {
+        opacity: 0.5;
+      }
+    }
+  }
+</style>
+
+<div class="pagination-display">
+  <ul class="pagination-display__list">
+    <li class="pagination-display__item">
+      <a
+        class="pagination-display__link pagination-display__link_active"
+        href="/mock-address/change-me">
+        1
+      </a>
+    </li>
+    <li class="pagination-display__item">
+      <a class="pagination-display__link" href="/mock-address/change-me">2</a>
+    </li>
+    <li class="pagination-display__item">
+      <a class="pagination-display__link" href="/mock-address/change-me">3</a>
+    </li>
+    <li class="pagination-display__item">
+      <a class="pagination-display__link" href="/mock-address/change-me">...</a>
+    </li>
+    <li class="pagination-display__item">
+      <a class="pagination-display__link" href="/mock-address/change-me">15</a>
+    </li>
+    <li class="pagination-display__item">
+      <a
+        class="pagination-display__link pagination-display__link_arrow"
+        href="/mock-address/change-me" />
+    </li>
+  </ul>
+</div>
