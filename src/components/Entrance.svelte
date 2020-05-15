7e97a944844901d8d7111e72b6a2afe5fd6dfcd4
Alan Reid <alanreidt@gmail.com>
2020-05-15 13:35:33 +0300

    Convert Entrance component

---
+++ src/components/Entrance.svelte
@@ -0,0 +1,131 @@
+<script>
+  import Button from "./Button.svelte";
+
+  const userName = "cosmin negoita";
+</script>
+
+<style lang="less">
+  @import "../styles/variables";
+  @import "../styles/mixins";
+
+  @button-border-width: 2px;
+  @username-height: 30px;
+
+  .entrance {
+    @media (max-width: 1050px) {
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+
+    &__item {
+      display: none;
+
+      &_is-active {
+        display: block;
+      }
+    }
+
+    &__button-group {
+      display: flex;
+      flex-flow: row nowrap;
+
+      @media (max-width: 1050px) {
+        flex-flow: column nowrap;
+        align-items: center;
+      }
+    }
+
+    &__button-wrapper {
+      margin-right: 20px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      @media (max-width: 1050px) {
+        margin-right: 0;
+        margin-bottom: 10px;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+    }
+
+    &__button {
+      @button-height: 34px;
+
+      height: @button-height;
+      line-height: @button-height - 2 * @button-border-width;
+
+      &_main {
+        line-height: @button-height;
+      }
+    }
+
+    &__username-wrapper {
+      display: block;
+      width: 100%;
+      box-sizing: border-box;
+
+      @media (max-width: 1050px) {
+        text-align: center;
+      }
+    }
+
+    &__username {
+      .not-selectable();
+      .body-text();
+      margin-top: 0;
+      margin-bottom: 0;
+
+      display: inline-block;
+      vertical-align: middle;
+      height: @username-height;
+      line-height: @username-height;
+      padding-left: 30px;
+
+      color: #colors[dark-shade-50];
+      border-left: 1px solid rgba(31, 32, 65, 0.1);
+      text-transform: capitalize;
+
+      box-sizing: border-box;
+
+      @media (max-width: 1050px) {
+        height: auto;
+        padding-left: 0;
+        padding-top: 10px;
+
+        line-height: inherit;
+        border-left: 0;
+        border-top: 1px solid rgba(31, 32, 65, 0.1);
+      }
+    }
+  }
+</style>
+
+<div class="entrance">
+  <div class="entrance__item entrance__item_is-active">
+    <div class="entrance__button-group">
+      <div class="entrance__button-wrapper">
+        <Button
+          class="button entrance__button"
+          text="Войти"
+          mods="secondary"
+          href="../../../src/markup/pages/sign-in.pug" />
+      </div>
+      <div class="entrance__button-wrapper">
+        <Button
+          class="button entrance__button entrance__button_main"
+          text="Зарегистрироваться"
+          mods="main"
+          href="../../../src/markup/pages/registration.pug" />
+      </div>
+    </div>
+  </div>
+  <div class="entrance__item">
+    <div class="entrance__username-wrapper">
+      <span class="entrance__username">{userName}</span>
+    </div>
+  </div>
+</div>
