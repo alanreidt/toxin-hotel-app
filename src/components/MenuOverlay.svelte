6fbf0ede81026890bc09bbd636997cccfd6d5369
Alan Reid <alanreidt@gmail.com>
2020-05-15 13:35:50 +0300

    Convert MenuOverlay component

---
+++ src/components/MenuOverlay.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<style lang="less">
+  @import "../styles/variables";
+  @import "../styles/mixins";
+
+  .menu-overlay {
+    .block-default-styles();
+
+    position: fixed;
+    z-index: 700;
+    top: 0;
+    left: 0;
+
+    background-color: rgba(0, 0, 0, 0.2);
+
+    pointer-events: none;
+    opacity: 0;
+
+    transition: opacity 0.3s ease;
+    will-change: opacity;
+
+    &_is-active {
+      pointer-events: auto;
+      opacity: 1;
+    }
+  }
+</style>
+
+<div class="menu-overlay" id="menu-overlay"></div>
