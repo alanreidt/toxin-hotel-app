a18d7bf36aef5ead3a369d5ffd0b963ef676d30b
Alan Reid <alanreidt@gmail.com>
2020-05-16 21:20:25 +0300

    Derive Expander component from Dropdown

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Expander from "./Expander.svelte";
   import InputField from "./InputField.svelte";
   import DropdownMenu from "./DropdownMenu.svelte";
 
@@ -22,109 +23,22 @@
       .filter((item) => Number(item[0]) !== 0)
       .join(", ");
   }
-
-  let isExpanded = false;
-
-  function toggleIsExpanded() {
-    isExpanded = !isExpanded;
-  }
-
-  const SPACE_KEY = " ";
-
-  function handleTriggerKeydown(event) {
-    if (event.key === "Enter" || event.key === SPACE_KEY) {
-      event.preventDefault();
-
-      toggleIsExpanded();
-    }
-
-    if (event.key === "Escape" || event.key === "Esc") {
-      event.preventDefault();
-
-      isExpanded = false;
-    }
-  }
-
-  function handleTriggerClick() {
-    toggleIsExpanded();
-  }
 </script>
 
 <style lang="less">
   @import "../styles/variables";
   @import "../styles/mixins";
-
-  .dropdown {
-    position: relative;
-    z-index: 200;
-
-    display: inline-block;
-    vertical-align: middle;
-    width: 100%;
-
-    &_is-expanded &__trigger {
-      color: #colors[dark-shade-75];
-    }
-
-    &__trigger {
-      position: relative;
-      color: #colors[dark-shade-50];
-
-      &::after {
-        .material-icons();
-        font-size: 1.5rem;
-
-        content: "\e5cf";
-        position: absolute;
-        bottom: 0;
-        right: 0;
-
-        display: inline-block;
-        vertical-align: middle;
-
-        width: 2.75rem;
-        height: 2.75rem;
-        line-height: 2.75rem;
-
-        text-align: center;
-        color: inherit;
-        background-color: transparent;
-
-        box-sizing: border-box;
-        pointer-events: none;
-      }
-    }
-
-    &_is-expanded &__menu {
-      pointer-events: auto;
-      opacity: 1;
-    }
-
-    &__menu {
-      position: absolute;
-      z-index: 200;
-      width: 100%;
-      top: 100%;
-      left: 0;
-
-      opacity: 0;
-      pointer-events: none;
-    }
-  }
 </style>
 
-<div class="dropdown {isExpanded ? 'dropdown_is-expanded' : ''}">
-  <div
-    class="dropdown__trigger"
-    on:keydown={handleTriggerKeydown}
-    on:click={handleTriggerClick}>
+<Expander>
+  <div slot="trigger">
     <InputField
       value={inputValue}
       readonly={true}
       {labelText}
       {...$$restProps} />
   </div>
-  <div class="dropdown__menu">
+  <div slot="menu">
     <DropdownMenu bind:options {hasButtonPane} />
   </div>
-</div>
+</Expander>
---
+++ src/components/Expander.svelte
@@ -0,0 +1,102 @@
+<script>
+  let isExpanded = false;
+
+  function toggleIsExpanded() {
+    isExpanded = !isExpanded;
+  }
+
+  const SPACE_KEY = " ";
+
+  function handleTriggerKeydown(event) {
+    if (event.key === "Enter" || event.key === SPACE_KEY) {
+      event.preventDefault();
+
+      toggleIsExpanded();
+    }
+
+    if (event.key === "Escape" || event.key === "Esc") {
+      event.preventDefault();
+
+      isExpanded = false;
+    }
+  }
+
+  function handleTriggerClick() {
+    toggleIsExpanded();
+  }
+</script>
+
+<style lang="less">
+  @import "../styles/variables";
+  @import "../styles/mixins";
+
+  .expander {
+    position: relative;
+    z-index: 200;
+
+    display: inline-block;
+    vertical-align: middle;
+    width: 100%;
+
+    &_is-expanded &__trigger {
+      color: #colors[dark-shade-75];
+    }
+
+    &__trigger {
+      position: relative;
+      color: #colors[dark-shade-50];
+
+      &::after {
+        .material-icons();
+        font-size: 1.5rem;
+
+        content: "\e5cf";
+        position: absolute;
+        bottom: 0;
+        right: 0;
+
+        display: inline-block;
+        vertical-align: middle;
+
+        width: 2.75rem;
+        height: 2.75rem;
+        line-height: 2.75rem;
+
+        text-align: center;
+        color: inherit;
+        background-color: transparent;
+
+        box-sizing: border-box;
+        pointer-events: none;
+      }
+    }
+
+    &_is-expanded &__menu {
+      pointer-events: auto;
+      opacity: 1;
+    }
+
+    &__menu {
+      position: absolute;
+      z-index: 200;
+      width: 100%;
+      top: 100%;
+      left: 0;
+
+      opacity: 0;
+      pointer-events: none;
+    }
+  }
+</style>
+
+<div class="expander {isExpanded ? 'expander_is-expanded' : ''}">
+  <div
+    class="expander__trigger"
+    on:keydown={handleTriggerKeydown}
+    on:click={handleTriggerClick}>
+    <slot name="trigger"></slot>
+  </div>
+  <div class="expander__menu">
+    <slot name="menu"></slot>
+  </div>
+</div>
