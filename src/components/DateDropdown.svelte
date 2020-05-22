76cd06d95a9f85872bab1b4ecc3120ad966772d2
Alan Reid <alanreidt@gmail.com>
2020-05-22 13:11:39 +0300

    Set up the arrows for DateDropdown component

--- src/components/DateDropdown.svelte
+++ src/components/DateDropdown.svelte
@@ -35,17 +35,50 @@
         margin-right: 0;
       }
     }
+
+    &__input {
+      position: relative;
+      color: #colors[dark-shade-50];
+
+      &::after {
+        .material-icons();
+        font-size: 1.5rem;
+
+        content: "\e5cf";
+        position: absolute;
+        bottom: 0;
+        right: 0;
+
+        display: inline-block;
+        vertical-align: middle;
+
+        width: 2.75rem;
+        height: 2.75rem;
+        line-height: 2.75rem;
+
+        text-align: center;
+        color: inherit;
+        background-color: transparent;
+
+        box-sizing: border-box;
+        pointer-events: none;
+      }
+    }
+
+    &__datepicker {
+      padding-top: 5px;
+    }
   }
 </style>
 
 <div class="date-dropdown">
   <Expander>
     <div on:keydown|stopPropagation on:click|stopPropagation class="date-dropdown__input-group" slot="trigger">
-      <div class="date-dropdown__col">
-        <DateInputField labelText="date dropdown" />
+      <div class="date-dropdown__col date-dropdown__input">
+        <DateInputField class="date-dropdown__input" labelText="date dropdown" />
       </div>
-      <div class="date-dropdown__col">
-        <DateInputField labelText="date dropdown" />
+      <div class="date-dropdown__col date-dropdown__input">
+        <DateInputField class="date-dropdown__input" labelText="date dropdown" />
       </div>
     </div>
     <div class="date-dropdown__datepicker" slot="menu">
