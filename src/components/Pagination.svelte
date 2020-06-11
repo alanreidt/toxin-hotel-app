a9fe7c1f787bb1ce24a366d9652e41e629b82504
Alan Reid <alanreidt@gmail.com>
2020-06-11 12:13:39 +0300

    Add Pagination component logic

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -1,5 +1,31 @@
 <script>
   import PaginationDisplay from "./PaginationDisplay.svelte";
+
+  export let itemsPerPage = 12;
+  export let itemsInTotal = 100;
+  export let currentPage = 1;
+
+  const roundNumberBy = function(number, boundary) {
+    if (number < boundary) {
+      return number;
+    }
+
+    const result = Math.floor(number / boundary) * boundary;
+
+    return `${result}+`;
+  }
+
+  const roundedTotal = roundNumberBy(itemsInTotal, 100);
+
+  const findIndex = function(currentPage, itemsPerPage, start = 0) {
+    return start + (currentPage - 1) * itemsPerPage;
+  }
+
+  $: currentIndex = findIndex(currentPage, itemsPerPage + 1);
+  $: firstItemIndex = Math.max(1, currentIndex);
+  $: lastItemIndex = currentIndex + itemsPerPage;
+
+  $: text = `${firstItemIndex}—${lastItemIndex} из ${roundedTotal} вариантов аренды`;
 </script>
 
 <style lang="less">
@@ -20,5 +46,5 @@
 
 <div class="pagination">
   <PaginationDisplay/>
-  <p class="pagination__text">1–12 из 100+ вариантов аренды</p>
+  <p class="pagination__text">{text}</p>
 </div>
