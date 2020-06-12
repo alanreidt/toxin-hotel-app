bc35af539852e1609406a51590f6f4c5219b9b39
Alan Reid <alanreidt@gmail.com>
2020-06-12 10:47:36 +0300

    Add View logic to PaginationDisplay component

--- src/components/PaginationDisplay.svelte
+++ src/components/PaginationDisplay.svelte
@@ -1,4 +1,19 @@
 <script>
+  export let itemsPerPage = 12;
+  export let itemsInTotal = 100;
+  export let currentPage = 1;
+  export let maxPageQuantity = 5;
+  export let pagesToDisplay = 3;
+
+  const pagesInTotal = Math.ceil(itemsInTotal / itemsPerPage);
+  const pageNumbers = new Array(pagesInTotal).fill(1).map((value, index) => index + 1);
+
+  const isMaxPageQuantityExceeded = pagesInTotal > maxPageQuantity;
+  const currentPageIndex = currentPage - 1;
+
+  $: displayedPageNumbers = isMaxPageQuantityExceeded ?
+    pageNumbers.slice(currentPageIndex, currentPageIndex + pagesToDisplay) :
+    pageNumbers;
 </script>
 
 <style lang="less">
@@ -72,29 +87,33 @@
 
 <div class="pagination-display">
   <ul class="pagination-display__list">
-    <li class="pagination-display__item">
-      <a
-        class="pagination-display__link pagination-display__link_active"
-        href="/mock-address/change-me">
-        1
-      </a>
-    </li>
-    <li class="pagination-display__item">
-      <a class="pagination-display__link" href="/mock-address/change-me">2</a>
-    </li>
-    <li class="pagination-display__item">
-      <a class="pagination-display__link" href="/mock-address/change-me">3</a>
-    </li>
-    <li class="pagination-display__item">
-      <a class="pagination-display__link" href="/mock-address/change-me">...</a>
-    </li>
-    <li class="pagination-display__item">
-      <a class="pagination-display__link" href="/mock-address/change-me">15</a>
-    </li>
+    {#each displayedPageNumbers as pageNumber}
+      <li class="pagination-display__item">
+        <a
+          class="pagination-display__link {pageNumber === currentPage ? "pagination-display__link_active" : ''}"
+          href="mock-address/change-me">
+          {pageNumber}
+        </a>
+      </li>
+    {/each}
+
+    {#if (isMaxPageQuantityExceeded)}
+      <li class="pagination-display__item">
+        <a class="pagination-display__link">...</a>
+      </li>
+      <li class="pagination-display__item">
+        <a
+          class="pagination-display__link"
+          href="mock-address/change-me">
+          {pageNumbers[pageNumbers.length - 1]}
+        </a>
+      </li>
+    {/if}
+
     <li class="pagination-display__item">
       <a
         class="pagination-display__link pagination-display__link_arrow"
-        href="/mock-address/change-me" />
+        href="/mock-address/change-me"></a>
     </li>
   </ul>
 </div>
