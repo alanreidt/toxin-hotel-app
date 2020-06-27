41dffa8f23eb6f8a3f7d46fb5e66bdac44ce7c53
Alan Reid <alanreidt@gmail.com>
2020-06-27 17:50:03 +0300

    Add layout template for pages

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Topline from "../components/Topline.svelte";
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<style lang="less">
+  //- This is a modification of code from https://github.com/philipwalton/solved-by-flexbox/blob/master/assets/css/components/site.css
+
+  /**
+  * 1. Avoid the IE 10-11 `min-height` bug.
+  * 2. Set `flex-shrink` to `0` to prevent some browsers from
+  *    letting these items shrink to smaller than their content's default
+  *    minimum size. See http://bit.ly/1Mn35US for details.
+  * 3. Use `%` instead of `vh` since `vh` is buggy in older mobile Safari.
+  */
+
+  .page {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+
+    /* margin: 0; */
+    box-sizing: border-box;
+
+    &__header,
+    &__footer {
+      flex: none; /* 2 */
+    }
+
+    &__content {
+      flex: 1 0 auto; /* 2 */
+    }
+  }
+</style>
+
+<div class="page">
+  <div class="page__header">
+    <Topline />
+  </div>
+
+  <main class="page__content">
+    <slot />
+  </main>
+
+  <div class="page__footer">
+    <Footer />
+  </div>
+</div>
