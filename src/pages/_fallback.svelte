33930da762fc81a20183c734f25a5bc1f6a19d12
Alan Reid <alanreidt@gmail.com>
2020-06-27 18:26:17 +0300

    Add _fallback page for 404 code error

---
+++ src/pages/_fallback.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { url } from '@sveltech/routify'
+</script>
+
+<style>
+  .e404 {
+    text-align: center;
+  }
+
+  .huge {
+    font-size: 12rem;
+  }
+
+  .big {
+    font-size: 2rem;
+  }
+</style>
+
+<div class="e404">
+  <div class="huge">404</div>
+  <div class="big">Page not found.
+    <!-- link to the parent folder of _fallback.svelte -->
+    <a href={$url('../')}>Go back</a>
+  </div>
+</div>
