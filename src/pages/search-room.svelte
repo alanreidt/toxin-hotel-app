4d18ddd15deea7c216f533ecce5415dda85e0ff5
Alan Reid <alanreidt@gmail.com>
2020-07-02 13:10:09 +0300

    Create search-room page

---
+++ src/pages/search-room.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Search from "../components/Search.svelte";
+
+  import { metatags } from '@sveltech/routify';
+
+  metatags.title = "Найдите номер под ваши желания";
+  metatags.description = "Change me";
+</script>
+
+<!-- routify:options name="search-room" index=false -->
+
+<Search />
