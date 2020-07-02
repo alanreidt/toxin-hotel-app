1e12a590a10f8fa85e2e3bb29f4cd8df521a68a3
Alan Reid <alanreidt@gmail.com>
2020-07-02 11:15:45 +0300

    Add index page metatags

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,5 +1,10 @@
 <script>
   import Hero from "../components/Hero.svelte";
+
+  import { metatags } from '@sveltech/routify';
+
+  metatags.title = "Лучшие номера для вашей работы, отдыха и просто вдохновения";
+  metatags.description = "Change me";
 </script>
 
 <!-- routify:options name="index" index=false -->
