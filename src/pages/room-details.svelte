5d18e0830dcf3653ef55d4d56654c8a1dacd0f47
Alan Reid <alanreidt@gmail.com>
2020-07-03 13:27:38 +0300

    Create room-details page

---
+++ src/pages/room-details.svelte
@@ -0,0 +1,20 @@
+<script>
+  import RoomDetailsHead from "../components/RoomDetailsHead.svelte";
+  import RoomDetailsBody from "../components/RoomDetailsBody.svelte";
+
+  import { metatags } from '@sveltech/routify';
+
+  metatags.title = "change-me";
+  metatags.description = "change-me";
+</script>
+
+<!-- routify:options name="room-details" index=false -->
+
+<div class="room-details">
+  <div class="room-details__head">
+    <RoomDetailsHead />
+  </div>
+  <div class="room-details__body">
+    <RoomDetailsBody />
+  </div>
+</div>
