9e3157c3d0ad41fa2c1a790129015852e483c0d7
Alan Reid <alanreidt@gmail.com>
2020-07-03 13:24:20 +0300

    Create RoomDetailsHead component

---
+++ src/components/RoomDetailsHead.svelte
@@ -0,0 +1,157 @@
+<script>
+</script>
+
+<style lang="less">
+  @import "../styles/variables";
+  @import "../styles/mixins";
+
+  .room-details-head {
+    .block-default-styles();
+    height: 33vw;
+
+    @media (max-width: @site-width) {
+      height: 35vw;
+    }
+
+    @media (max-width: 1050px) {
+      height: 37vw;
+    }
+
+    @media (max-width: 833px) {
+      height: 39vw;
+    }
+
+    @media (max-width: 760px) {
+      height: 40vh;
+      min-height: 340px;
+    }
+
+    @media (max-width: 540px) {
+      height: 25vh;
+      min-height: 200px;
+    }
+
+    &__inner {
+      .block-default-styles();
+
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: flex-start;
+      align-items: stretch;
+
+      @media (max-width: 760px) {
+        flex-flow: column nowrap;
+      }
+
+      @media (max-width: 540px) {
+        flex-flow: row nowrap;
+      }
+    }
+
+    &__main-section {
+      flex: 1 1 0;
+
+      @media (max-width: 540px) {
+        flex: none;
+        width: 100%;
+      }
+    }
+
+    &__secondary-section {
+      width: 35%;
+
+      @media (max-width: @site-width) {
+        width: 40%;
+      }
+
+      @media (max-width: 1050px) {
+        width: 45%;
+      }
+
+      @media (max-width: 833px) {
+        width: 50%;
+      }
+
+      @media (max-width: 760px) {
+        width: 100%;
+        height: 40%;
+      }
+
+      @media (max-width: 540px) {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    &__section-inner {
+      .block-default-styles();
+
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: flex-start;
+      align-items: stretch;
+
+      @media (max-width: 760px) {
+        flex-flow: row nowrap;
+      }
+    }
+
+    &__section-item {
+      flex: 1 1 auto;
+
+      @media (max-width: 540px) {
+        flex: none;
+        width: 100%;
+      }
+    }
+
+    &__image {
+      .block-default-styles();
+
+      background: url("/images/room-details-888.jpg") center/cover
+        no-repeat;
+
+      &_first {
+        @media (max-width: 1050px) {
+          background-position: right;
+        }
+
+        @media (max-width: 833px) {
+          background-position: -220px center;
+        }
+
+        @media (max-width: 760px) {
+          background-position: bottom;
+        }
+      }
+
+      &_second {
+        background-image: url("/images/room-details-888-2.jpg");
+      }
+
+      &_third {
+        background-image: url("/images/room-details-888-3.jpg");
+      }
+    }
+  }
+</style>
+
+<div class="room-details-head">
+  <div class="room-details-head__inner">
+    <div class="room-details-head__main-section">
+      <div class="room-details-head__image room-details-head__image_first" />
+    </div>
+    <div class="room-details-head__secondary-section">
+      <div class="room-details-head__section-inner">
+        <div class="room-details-head__section-item">
+          <div
+            class="room-details-head__image room-details-head__image_second" />
+        </div>
+        <div class="room-details-head__section-item">
+          <div
+            class="room-details-head__image room-details-head__image_third" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
